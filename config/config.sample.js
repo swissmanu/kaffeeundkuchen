0a912ebc13e1faf1927969f52d2a9126d44f302d
Manuel Alabor <msites@msites.net>
2013-01-27 21:20:30 +0100

    comments for configsample

--- config/config.sample.js
+++ config/config.sample.js
@@ -1,7 +1,26 @@
-{
-	spotify: {
-		username: 'username'
-		,password: 'password'
-		,appkeyfile: 'config/spotify_appkey.key'	
-	}
+/** File: config.sample.js
+ * Create a copy of this file named "config.js" and modify it to your needs.
+ *
+ * Spotify Settings:
+ * This section contains all stuff related to Spotify. Login to spotify.com and
+ * make sure you have a Spotify premium account.
+ *
+ * Username & Password:
+ *  https://www.spotify.com/ch-de/account/set-device-password/
+ *  If not already done, you have to create a device password to use
+ *  Spotify with KaffeeUndKuchen. Follow the instructions on that page
+ *  and paste the regarding username and device password below.
+ * App Key:
+ *  https://developer.spotify.com/technologies/libspotify/keys/
+ *  Apply for an app key and download the binary version of it. Place it
+ *  in a proper place and make sure "appkeyfile" referes to that file.
+ */
+ module.exports = function() {
+	return {
+		spotify: {
+			username: 'username'
+			,password: 'password'
+			,appkeyfile: 'config/spotify_appkey.key'	
+		}
+	};
 }
