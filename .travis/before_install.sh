e9b8b067b4c6c43c2dcfba9776bfc1832271d189
Manuel Alabor <msites@msites.net>
2013-08-08 19:12:32 +0200

    added installation of libspotify system library to before_install script of travis. maybe this fixes the build

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -5,6 +5,14 @@ echo '# BEFORE INSTALL #'
 echo '# - START - #'
 echo '######################################'
 
+echo '---- Installing libspotify ----'
+cd /tmp
+wget https://developer.spotify.com/download/libspotify/libspotify-12.1.51-Linux-x86_64-release.tar.gz
+tar zxfv libspotify-12.1.51-Linux-x86_64-release.tar.gz
+cd libspotify-0.0.8-linux6-x86_64/
+sudo make install prefix=/usr/local
+sudo ldconfig
+
 echo '---- Installing node-jscoverage ----'
 cd /tmp
 git clone git://github.com/visionmedia/node-jscoverage.git
