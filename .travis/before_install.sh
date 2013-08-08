0dd6e26fb59af4086e4705c349b738aedb9f2c54
Manuel Alabor <msites@msites.net>
2013-08-08 19:18:41 +0200

    again travis before_install

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -9,7 +9,7 @@ echo '---- Installing libspotify ----'
 cd /tmp
 wget https://developer.spotify.com/download/libspotify/libspotify-12.1.51-Linux-x86_64-release.tar.gz
 tar zxfv libspotify-12.1.51-Linux-x86_64-release.tar.gz
-cd libspotify-0.0.8-linux6-x86_64/
+cd libspotify-12.1.51-Linux-x86_64-release/
 sudo make install prefix=/usr/local
 sudo ldconfig
 
