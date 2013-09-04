a7cfd9d9dd257192f126f104d9061cf28cdc5f25
Manuel Alabor <msites@msites.net>
2013-09-04 21:22:48 +0200

    satisfy lint

--- src/client/app/directives.js
+++ src/client/app/directives.js
@@ -3,13 +3,13 @@ var angular = require('./vendor/angular-shim.js');
 (function() {
 
 	function eatClick() {
-		return function(scope, element, attrs) {
+		return function(scope, element) {
 			element.bind('click', function(event) {
 				event.preventDefault();
 			});
-		}
+		};
 	}
 
 	angular.module('kuk.directives')
-		.directive('eatClick', eatClick)
+		.directive('eatClick', eatClick);
 })();
